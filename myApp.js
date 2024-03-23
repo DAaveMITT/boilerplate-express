7cfe65223ebe2bf1f94c0029b35e7c4646c7d492
Al_Gore <daavemitt@gmail.com>
2024-03-23 15:49:25 -0400

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -3,43 +3,4 @@ let app = express();
 
 app.get("/", function(req, res) {
     res.send("Hello Express");
-  });
-
-console.log("Hello World");
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-module.exports = app;
+});

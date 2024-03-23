a0c9ed212a02097e056a431a2dd0a5910339e74d
Al_Gore <daavemitt@gmail.com>
2024-03-23 15:50:33 -0400

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -1,6 +1,45 @@
 let express = require('express');
 let app = express();
 
-app.get("/", function(req, res) {
+app.get("/", (req, res) => {
     res.send("Hello Express");
-});
+  });
+
+console.log("Hello World");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = app;
